3659b8db19d2b5847802fd534c961d28f0a28f15
alainrollejr <35453310+alainrollejr@users.noreply.github.com>
2018-02-07 22:06:26 +0100

    adding the notion of office time delta

--- skydiagclk.py
+++ skydiagclk.py
@@ -13,7 +13,69 @@ import argparse
 import pandas as pd
 from dateutil.parser import parse
 
+from datetime import datetime
+from datetime import timedelta
 
+def adjust_hour_delta(t, start, stop):
+
+    start_hour = start.seconds//3600
+    end_hour = stop.seconds//3600
+    zero = timedelta(0)
+
+    if t - t.replace(hour = start_hour, minute = 0, second = 0) < zero:
+        t = t.replace(hour = start_hour, minute = 0, second = 0)
+    elif t - t.replace(hour = end_hour, minute = 0, second = 0) > zero:
+        t = t.replace(hour = end_hour, minute = 0, second = 0)
+    # Now get the delta
+    delta = timedelta(hours=t.hour, minutes=t.minute, seconds = t.second)
+
+    return delta    
+
+def full_in_between_working_days(a, b):
+    working = 0
+    b = b - timedelta(days=1)
+    while b.date() > a.date():
+        if b.weekday() < 5:
+            working += 1
+        b = b - timedelta(days=1)
+    return working
+
+def office_time_between(a, b, start = timedelta(hours = 8),
+                        stop = timedelta(hours = 17)):
+    """
+    Return the total office time between `a` and `b` as a timedelta
+    object. Office time consists of weekdays from `start` to `stop`
+    (default: 08:00 to 17:00).
+    """
+    zero = timedelta(0)
+    assert(zero <= start <= stop <= timedelta(1))
+    office_day = stop - start
+    working_days = full_in_between_working_days(a, b)
+
+    total = office_day * working_days
+    # Calculate the time adusted deltas for the the start and end days
+    a_delta = adjust_hour_delta(a, start, stop)
+    b_delta = adjust_hour_delta(b, start, stop)
+
+
+    if a.date() == b.date():
+        # If this was a weekend, ignore
+        if a.weekday() < 5:
+            total = total + b_delta - a_delta
+    else:
+        # We now consider if the start day was a weekend
+        if a.weekday() > 4:
+            a_worked = zero
+        else:
+            a_worked = stop - a_delta
+        # And if the end day was a weekend
+        if b.weekday() > 4:
+            b_worked = zero
+        else:
+            b_worked = b_delta - start
+        total = total + a_worked + b_worked
+
+    return total
 
 def main(argv):   
     """
@@ -38,7 +100,7 @@ def main(argv):
     
      
     # the dataframe column headers
-    columns = ['dateTime', 'issue','status','severity','from','assignee','assigned_company','delta_time','delta_time_spent_in_company']
+    columns = ['dateTime', 'issue','status','severity','from','assignee','assigned_company','delta_time','office_delta_time','delta_time_spent_in_company']
     df = pd.DataFrame(columns=columns)
     
 
@@ -102,16 +164,19 @@ def main(argv):
                     
                     if row_index_in_issue > 0:
                         delta_time = dateTime - prev_dateTime
+                        office_delta_time = office_time_between(prev_dateTime,dateTime)
                         print(delta_time)
+                        print(office_delta_time)
                     else:
                         delta_time = 0
+                        office_delta_time = 0
                     prev_dateTime = dateTime
                         
                     
                             
                     row=pd.Series([str(dateTime),str(issue_key),str(issue_status),
                                    str(issue_severity),str(from_str),str(to_mail),str(assigned_company),
-                                   str(delta_time),prev_company],columns)        
+                                   str(delta_time),str(office_delta_time),prev_company],columns)        
                     
                     
                     df = df.append([row],ignore_index=True)
